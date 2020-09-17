8bdffb65e6a69daea21b9274a728ad6ee04ce6d0
James Grieve <jamesonrgrieve@gmail.com>
2020-09-17 11:23:38 -0600

    In-Class practice;

--- 4Point1_EF/Program.cs
+++ 4Point1_EF/Program.cs
@@ -7,6 +7,26 @@ namespace _4Point1_EF
     {
         static void Main(string[] args)
         {
+            /* In-Class Practice
+             
+            -Perform this database setup in a new project with a "Manufacturer" table.
+            -Properties:
+                -ID (id, int(10), primary key, auto increment)
+                -Name (varchar(30), not null)
+                -YearFounded (int(10), null)
+             -Seed Data (Null founded date if not specified):
+                -Chevrolet, 1911
+                -Ford, 1903
+                -Tesla
+                -Dodge
+                -Toyota, 1937
+                -Honda, 1946
+                -Mitsubishi
+                -Nissan
+             */
+
+
+
             // Using "Code First" techniques allow us to make changes and even create entire databases without writing a single line of SQL.
             /*
 
