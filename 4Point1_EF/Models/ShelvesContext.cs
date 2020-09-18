b9f52168a4521d5f34499614afb8f5243f79b072
James Grieve <jamesonrgrieve@gmail.com>
2020-09-18 10:20:58 -0600

    Fixed a typo;

--- 4Point1_EF/Models/ShelvesContext.cs
+++ 4Point1_EF/Models/ShelvesContext.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Codee.Models
+namespace CodeFirstPractice.Models
 {
     public partial class ShelvesContext : DbContext
     {
